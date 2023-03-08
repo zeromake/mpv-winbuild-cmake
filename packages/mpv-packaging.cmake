0fd0a3b32acdd40276bab189b12b3aebd94e7a2d
zeromake <a390720046@gmail.com>
2023-03-08 09:31:16 +0800

    fix: pkg make

--- packages/mpv-packaging.cmake
+++ packages/mpv-packaging.cmake
@@ -3,13 +3,8 @@ file(WRITE ${PACKAGE}
 "#!/bin/bash
 7z x -y $1/d3dcompiler*.7z
 for dir in $2/mpv*$3*; do
-if [ -d $dir ] && [[ $dir =~ mpv-$3.*-git-* ]]; then
+if [ -d $dir ]; then
     cp -r $1/mpv-root/* $1/$3/d3dcompiler_43.dll $dir
-    7z a -m0=lzma2 -mx=9 -ms=on $dir.7z $dir/* -x!*.7z
-elif [ -d $dir ] && [[ $dir =~ mpv-.*$3$ ]]; then
-    cp -r $1/mpv-root/mpv $1/mpv-root/fonts $1/$3/d3dcompiler_43.dll $dir
-    7z a -m0=lzma2 -mx=9 -ms=on $dir.7z $dir/* -x!*.7z
-elif [ -d $dir ]; then
     7z a -m0=lzma2 -mx=9 -ms=on $dir.7z $dir/* -x!*.7z
 fi
 done")
